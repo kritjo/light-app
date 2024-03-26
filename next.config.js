7911abf4824f47077779ad3f5330535032841326
Kristian Tjelta Johansen <kritjo@kritjo.com>
2024-03-26 12:32:42 +0100

    Config relevant directories for eslint

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  eslint: {
+    dirs: ["app", "components", "db", "server"], // Only run ESLint on these directories during production builds (next build) and (next lint)
+  },
+};
