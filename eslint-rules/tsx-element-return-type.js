cc393befe5d5800397d1f3d5a11adaa6401282ec
Kristian Tjelta Johansen <kritjo@kritjo.com>
2024-03-27 14:11:51 +0100

    Must return ReactElements

---
+++ eslint-rules/tsx-element-return-type.js
@@ -0,0 +1,17 @@
+module.exports = {
+  create(context) {
+    if (!context.filename.endsWith(".tsx")) {
+      return {};
+    }
+    return {
+      ExportDefaultDeclaration(node) {
+        if (node.declaration?.returnType?.typeAnnotation.typeName.name !== "ReactElement") {
+          context.report({
+            node,
+            message: "Element must return a ReactElement"
+          });
+        }
+      }
+    };
+  }
+};
