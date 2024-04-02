d732627df7ab6f77052d8e94220fc1be30533c26
Kristian Tjelta Johansen <kritjo@kritjo.com>
2024-04-02 13:21:56 +0200

    Lint actions also

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   eslint: {
     // Only run ESLint on these directories during production builds (next build) and (next lint)
-    dirs: ["app", "components", "db", "server"],
+    dirs: ["actions", "app", "components", "db", "server"],
   },
   reactStrictMode: true,
   experimental: {
