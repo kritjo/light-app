78b9fb484468c7241fec6571d9f90da51db3b387
Kristian Tjelta Johansen <kritjo@kritjo.com>
2024-04-02 12:20:35 +0200

    Block comments before useserver not supported

--- eslint-plugin-kritjo-lightapp/lib/rules/use-server.js
+++ eslint-plugin-kritjo-lightapp/lib/rules/use-server.js
@@ -5,7 +5,7 @@ module.exports = {
         const lines = context.sourceCode.getText().split("\n");
         // Skip comments at the beginning of the file
         let line = lines.shift();
-        while (line.startsWith("//") || line.startsWith("/*")) {
+        while (line.startsWith("//")) {
           line = lines.shift();
         }
 
