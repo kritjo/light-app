f80a17cbd39fdc36fdc11736b8df0df07767c570
Kristian Tjelta Johansen <kritjo@kritjo.com>
2024-04-02 12:58:56 +0200

    Enable strict mode and typed routes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+/** @type {import("next").NextConfig} */
+const nextConfig = {
   eslint: {
-    dirs: ["app", "components", "db", "server"], // Only run ESLint on these directories during production builds (next build) and (next lint)
+    // Only run ESLint on these directories during production builds (next build) and (next lint)
+    dirs: ["app", "components", "db", "server"],
+  },
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
   },
 };
+
+module.exports = nextConfig;
